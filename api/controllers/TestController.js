1e6be419b475e1a7fc7168347c02d0cf30364892
Smrutiranjan Sahu <lee2smsahu@gmail.com>
2016-02-25 20:04:11 +0530

    typescript with twitter data service working

---
+++ api/controllers/TestController.js
@@ -0,0 +1,18 @@
+/// <reference path="../../typings/tsd.d.ts" />
+/**
+ * TestController
+ *
+ * @description :: Server-side logic for managing Tests
+ * @help 		:: See http://links.sailsjs.org/docs/controllers
+ */
+"use strict";
+var TestController = (function () {
+    function TestController() {
+    }
+    TestController.prototype.index = function (req, res) {
+        res.status(200).send('OK1');
+    };
+    return TestController;
+}());
+exports.TestController = TestController;
+module.exports = new TestController();
